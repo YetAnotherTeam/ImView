c6b60594ac18cd17d80f6ca674e2ef4a9d555136
Andrey Bondar <Alanai1995@gmail.com>
2015-12-01 18:44:28 +0300

    init

---
+++ app/src/main/java/jat/rk2/ImageActivity.java
@@ -0,0 +1,25 @@
+package jat.rk2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ImageActivity extends DrawerActivity {
+
+    @Override
+    public NavigationDrawerItem getCurrentNavDrawerItem() {
+        return NavigationDrawerItem.MAIN;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
