c768d1460c281ae4736ce95088207ec95a02270d
Bulat Khasanov <afti@yandex.ru>
2016-01-19 05:40:50 +0300

    image new begin

--- app/src/main/java/jat/imview/ui/activity/ImageNewActivity.java
+++ app/src/main/java/jat/imview/ui/activity/ImageNewActivity.java
@@ -1,12 +1,58 @@
 package jat.imview.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 
-public class ImageNewActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+import jat.imview.R;
+
+public class ImageNewActivity extends BaseActivity implements View.OnClickListener {
+    private static final int REQUEST_CODE_SELECT_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_new);
+        findViewById(R.id.select_image_button).setOnClickListener(this);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case REQUEST_CODE_SELECT_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImageUri = imageReturnedIntent.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(selectedImageUri, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String filePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    Bitmap selectedImage = BitmapFactory.decodeFile(filePath);
+                }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.select_image_button:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Выберите изображение"), REQUEST_CODE_SELECT_IMAGE);
+                break;
+            case R.id.publish_button:
+
+                break;
+        }
     }
 }
